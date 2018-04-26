021ce70ef22f067188c280d892d1e8aca242d47c
AndersJuul <andersjuulsfirma@gmail.com>
2018-04-26 14:58:14 +0200

    a

---
+++ Src/Ajf.CoreSolver.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Ajf.CoreSolver.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Src/Ajf.CoreSolver.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ajf.CoreSolver.WebApi.WebApiApplication" Language="C#" %>
