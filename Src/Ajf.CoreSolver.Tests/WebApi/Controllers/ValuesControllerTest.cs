344b0ab5400fe220781d100c6008bec0fe6267af
AndersJuul <andersjuulsfirma@gmail.com>
2018-04-28 22:51:36 +0200

    a

--- Src/Ajf.CoreSolver.Tests/WebApi/Controllers/ValuesControllerTest.cs
+++ Src/Ajf.CoreSolver.Tests/WebApi/Controllers/ValuesControllerTest.cs
@@ -9,34 +9,34 @@ namespace Ajf.CoreSolver.WebApi.Tests.Controllers
     [TestFixture]
     public class ValuesControllerTest
     {
-        [Test]
-        public void Get()
-        {
-            // Arrange
-            CalculationController controller = new CalculationController();
+        //[Test]
+        //public void Get()
+        //{
+        //    // Arrange
+        //    CalculationController controller = new CalculationController();
 
-            // Act
-            IEnumerable<string> result = controller.Get();
+        //    // Act
+        //    IEnumerable<string> result = controller.Get();
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count());
-            Assert.AreEqual("value1", result.ElementAt(0));
-            Assert.AreEqual("value2", result.ElementAt(1));
-        }
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(2, result.Count());
+        //    Assert.AreEqual("value1", result.ElementAt(0));
+        //    Assert.AreEqual("value2", result.ElementAt(1));
+        //}
 
-        [Test]
-        public void GetById()
-        {
-            // Arrange
-            CalculationController controller = new CalculationController();
+        //[Test]
+        //public void GetById()
+        //{
+        //    // Arrange
+        //    CalculationController controller = new CalculationController();
 
-            // Act
-            string result = controller.Get(5);
+        //    // Act
+        //    string result = controller.Get(5);
 
-            // Assert
-            Assert.AreEqual("value", result);
-        }
+        //    // Assert
+        //    Assert.AreEqual("value", result);
+        //}
 
         [Test]
         public void Post()
@@ -50,28 +50,28 @@ namespace Ajf.CoreSolver.WebApi.Tests.Controllers
             // Assert
         }
 
-        [Test]
-        public void Put()
-        {
-            // Arrange
-            CalculationController controller = new CalculationController();
+        //[Test]
+        //public void Put()
+        //{
+        //    // Arrange
+        //    CalculationController controller = new CalculationController();
 
-            // Act
-            controller.Put(5, "value");
+        //    // Act
+        //    controller.Put(5, "value");
 
-            // Assert
-        }
+        //    // Assert
+        //}
 
-        [Test]
-        public void Delete()
-        {
-            // Arrange
-            CalculationController controller = new CalculationController();
+        //[Test]
+        //public void Delete()
+        //{
+        //    // Arrange
+        //    CalculationController controller = new CalculationController();
 
-            // Act
-            controller.Delete(5);
+        //    // Act
+        //    controller.Delete(5);
 
-            // Assert
-        }
+        //    // Assert
+        //}
     }
 }
--- Src/Ajf.CoreSolver.WebApi/Controllers/CalculationController.cs
+++ Src/Ajf.CoreSolver.WebApi/Controllers/CalculationController.cs
@@ -12,38 +12,16 @@ namespace Ajf.CoreSolver.WebApi.Controllers
 {
     public class CalculationController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            Log.Logger.Debug("Get() called");
-
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            Log.Logger.Debug("Get(int id) called");
-
-            return "value";
-        }
-
-        // POST api/values
         public IHttpActionResult Post([FromBody]CalculationRequest calculationRequest)
         {
             Log.Logger.Debug("CalculationRequest : {@CalculationRequest}", calculationRequest);
 
-            return Ok(new CalculationResponse());
+            var calculationResponse = new CalculationResponse();
+
+            Log.Logger.Debug("Returning : {@CalculationResponse}", calculationResponse);
+
+            return Ok(calculationResponse);
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
