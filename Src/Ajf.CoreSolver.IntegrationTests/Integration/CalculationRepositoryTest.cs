66f07eb9e16c1121bb54300a14cb119dca31e397
AndersJuul <andersjuulsfirma@gmail.com>
2018-05-01 19:02:55 +0200

    a

--- Src/Ajf.CoreSolver.IntegrationTests/Integration/CalculationRepositoryTest.cs
+++ Src/Ajf.CoreSolver.IntegrationTests/Integration/CalculationRepositoryTest.cs
@@ -1,30 +1,30 @@
-﻿//using Ajf.CoreSolver.Models;
-//using Ajf.CoreSolver.Shared;
-//using Ajf.CoreSolver.Tests.Base;
-//using AutoFixture;
-//using NUnit.Framework;
+﻿using Ajf.CoreSolver.IntegrationTests.Base;
+using Ajf.CoreSolver.Models;
+using Ajf.CoreSolver.Shared;
+using AutoFixture;
+using NUnit.Framework;
 
-//namespace Ajf.CoreSolver.Tests.Integration
-//{
-//    [TestFixture]
-//    public class CalculationRepositoryTest : BaseIntegrationTestWithDb
-//    {
-//        [Test]
-//        [Category("Integration")]
-//        public void ThatCalculationCanBeInsertedAndRetrieved()
-//        {
-//            // Arrange
-//            var dbContextProviderForTest = new DbContextProviderForTest(ConnectionString);
-//            var sut = new CalculationRepository(dbContextProviderForTest);
-//            var calculationRequest = Fixture
-//                .Build<CalculationRequest>()
-//                .Create();
+namespace Ajf.CoreSolver.IntegrationTests.Integration
+{
+    [TestFixture]
+    public class CalculationRepositoryTest : BaseIntegrationTestWithDb
+    {
+        [Test]
+        [Category("Integration")]
+        public void ThatCalculationCanBeInsertedAndRetrieved()
+        {
+            // Arrange
+            var dbContextProviderForTest = new DbContextProviderForTest(ConnectionString);
+            var sut = new CalculationRepository(dbContextProviderForTest);
+            var calculationRequest = Fixture
+                .Build<CalculationRequest>()
+                .Create();
 
-//            // Act
-//            sut.InsertCalculation(calculationRequest);
+            // Act
+            sut.InsertCalculation(calculationRequest);
 
-//            // Assert
-//            //Assert.IsTrue(validationResult.IsValid);
-//        }
-//    }
-//}
+            // Assert
+            //Assert.IsTrue(validationResult.IsValid);
+        }
+    }
+}
