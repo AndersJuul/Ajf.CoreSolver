c98f29033f51a455a8b063ba5d9dd97ea0169fa5
AndersJuul <andersjuulsfirma@gmail.com>
2018-05-01 18:45:51 +0200

    a

--- Src/Ajf.CoreSolver.IntegrationTests/Integration/IntegrationTest.cs
+++ Src/Ajf.CoreSolver.IntegrationTests/Integration/IntegrationTest.cs
@@ -1,32 +1,32 @@
-﻿//using System.Net;
-//using Ajf.CoreSolver.Models;
-//using Ajf.CoreSolver.Tests.Base;
-//using NUnit.Framework;
-//using RestSharp;
-//using AutoFixture;
+﻿using System.Net;
+using Ajf.CoreSolver.IntegrationTests.Base;
+using Ajf.CoreSolver.Models;
+using AutoFixture;
+using NUnit.Framework;
+using RestSharp;
 
-//namespace Ajf.CoreSolver.Tests.Integration
-//{
-//    [TestFixture]
-//    public class IntegrationTest:BaseIntegrationTest
-//    {
-//        [Test]
-//        [Category("Integration")]
-//        public void ThatPostingValidCalculationIsSucces()
-//        {
-//            // Arrange
-//            var client = GetRestClient();
-//            var request = new RestRequest("api/calculation", Method.POST)
-//            {
-//                RequestFormat = DataFormat.Json
-//            };
-//            request.AddBody(Fixture.Build< CalculationRequest>().Create()); 
+namespace Ajf.CoreSolver.IntegrationTests.Integration
+{
+    [TestFixture]
+    public class IntegrationTest : BaseIntegrationTest
+    {
+        [Test]
+        [Category("Integration")]
+        public void ThatPostingValidCalculationIsSucces()
+        {
+            // Arrange
+            var client = GetRestClient();
+            var request = new RestRequest("api/calculation", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(Fixture.Build<CalculationRequest>().Create());
 
-//            // Act
-//            var response = client.Execute<CalculationResponse>(request);
-            
-//            // Assert
-//            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-//        }
-//    }
-//}
+            // Act
+            var response = client.Execute<CalculationResponse>(request);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
