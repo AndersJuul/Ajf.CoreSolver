f412e752997f40864cbe7939ec72ead0318c3cc9
AndersJuul <andersjuulsfirma@gmail.com>
2018-05-01 15:30:03 +0200

    a

---
+++ Src/Ajf.CoreSolver.Tests/DummyFixture.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Ajf.CoreSolver.Tests
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void ThatTrue()
+        {
+        }
+    }
+}
