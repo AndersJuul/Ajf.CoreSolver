d954e6d0555529a9b7d6fb8c8f7f5d48cfcea7d0
AndersJuul <andersjuulsfirma@gmail.com>
2018-05-02 09:54:01 +0200

    a

--- Src/Ajf.CoreSolver.Shared/DbContextProvider.cs
+++ Src/Ajf.CoreSolver.Shared/DbContextProvider.cs
@@ -1,7 +1,19 @@
-﻿using Ajf.CoreSolver.DbModels;
+﻿using System.Diagnostics.CodeAnalysis;
+using Ajf.CoreSolver.DbModels;
 
 namespace Ajf.CoreSolver.Shared
 {
+    /// <summary>
+    /// Returns a DbContext based on config settings
+    /// -- the 'normal use' in the application. This
+    /// functionality, though simple, has been stubbed out
+    /// to allow for an alternative implementation of interface
+    /// to be used in automated tests. As encapsulaton of
+    /// hard-to-test code, it makes no sense to include it
+    /// in coverage. Correct behaviour is instead ensured
+    /// by code review.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
     public class DbContextProvider : IDbContextProvider
     {
         public CoreSolverContext GetDbContext()
